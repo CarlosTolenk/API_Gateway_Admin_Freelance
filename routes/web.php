139512e5df058f3db702612095053ba3bab5100e
Carlos Tolentino <carlos.tolentinoe@gmail.com>
2019-02-26 20:08:26 -0400

    first commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/', 'welcome');
+
+// Route::get('/', function () {
+//     echo 'EDteam';
+//     echo "<br />";
+//     echo Hash::make('EDteam');
+//     echo "<br />";
+//     echo "<br />";
+//     echo 'secret';
+//     echo "<br />";
+//     echo bcrypt('secret');
+//     echo "<br />";
+//     echo "<br />";    
+//     $cadena = 'larevel'; 
+//     echo $cadena;
+//     echo "<br />";
+//     echo Crypt::encrypt($cadena);
+//     echo "<br />";
+//     echo "<br />";
+//     echo 'Carlos';
+//     echo "<br />";
+//     echo md5('Carlos');
+//     echo "<br />";
+//     echo "<br />";
+//     // echo Crypt::decrypt($cadena);
+// });
